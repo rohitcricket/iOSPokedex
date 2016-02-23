96ddf35a09176293f14869af354e70d23357fba6
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro.local>
2016-02-23 15:55:22 -0800

    completed UICollectionView

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by ROHIT GUPTA on 2/23/16.
+//  Copyright © 2016 ROHIT GUPTA. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -16,7 +16,7 @@ class Pokemon {
         return _name
     }
     
-    var pokedex: Int {
+    var pokedexId: Int {
         return _pokedexId
     }
     
