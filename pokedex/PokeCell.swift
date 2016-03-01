b350fb80b84f57d6d0c56e6d121d1642f8a90f8a
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro.local>
2016-03-01 12:26:50 -0800

    fixed bugs

--- pokedex/PokeCell.swift
+++ pokedex/PokeCell.swift
@@ -9,5 +9,15 @@
 import UIKit
 
 class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
     
+    var pokemon: Pokemon!
+    
+    func configCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
 }
